87655ac8558d5f227d81806e097224f449d86e15
Abhishek Kumar <107580567+Abhishekkumarr2001@users.noreply.github.com>
2023-06-15 23:30:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function generateRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function changeBackgroundColor() {
+    var randomColor = generateRandomColor();
+    document.body.style.background = randomColor;
+  }
